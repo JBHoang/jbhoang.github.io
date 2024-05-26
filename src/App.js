bcfe6fa2679b55a45b743f1d60363c52cc8fe438
jason <jason_hoang@sfu.ca>
2024-05-26 03:11:42 -0700

    added description

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
       
       <img src ={profileImage} alt="profile-image" />
       <h2> About Me</h2>
-      
+      <p>I'm a Simon Fraser University Computer Science graduate with an interest in full-stack engineering. 
+        Besides building software, I love weight training, snowboarding and golfing.
+        An interesting fact about myself is a I'm a paramedic with BC Emgergency Health Service.
+        Check out my work experience and projects below.
+
+      </p>
       
     </div>
 
