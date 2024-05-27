085b846b73239efbc37648ac68b45fdf36293458
jason <jason_hoang@sfu.ca>
2024-05-27 01:19:21 -0700

    updated background, about me and navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import profileImage from './img/JasonPhoto.JPG'
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <div className="App">
-      <h1>Jason Hoang</h1>
       
-      <img src ={profileImage} alt="profile-image" />
-      <h2> About Me</h2>
-      <p>I'm a Simon Fraser University Computer Science graduate with an interest in full-stack engineering. 
-        Besides building software, I love weight training, snowboarding and golfing.
-        An interesting fact about myself is a I'm a paramedic with BC Emgergency Health Service.
-        Check out my work experience and projects below.
-
-      </p>
+      <>
+      <HeroSection/>
+      </>
+      
       
     </div>
 
