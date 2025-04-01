82d853e73abc5723d60391a871771807c15ff697
jason <jason_hoang@sfu.ca>
2025-03-31 22:46:35 -0700

    updated resume

--- src/components/projects.js
+++ src/components/projects.js
@@ -19,40 +19,7 @@ function projects() {
 
                 <div className="list_projects">  
 
-                <div className='tetris'>
-
-                    <div className="tetris-photo">
-                        <a href="https://www.jbhoang.com/TetrisGame/">
-                            <img className="tetris-logo" src ={tetrisImage} alt="profile-image" />
-                        </a>
-                    </div>
-
-                    <div>
-                        <h1 className="tetris-title">Tetris - Unity, C# </h1>
-                        <div className='project-link'>
-                            <ul>
-                                <li>
-                                    <a href="https://github.com/JBHoang/Tetris">
-                                        <div className='aligned'>
-                                            <img className="icon-logo" src ={gitImage} alt="profile-image" />
-                                            <span>SOURCE</span>
-                                        </div>
-                                    </a>
-                                </li>
-
-                                <li>
-                                <a href="https://www.jbhoang.com/TetrisGame/">
-                                    <div className='aligned'>
-                                        <img className="icon-logo" src ={externalLinkImage} alt="profile-image" />
-                                        <span>DEMO</span>
-                                    </div>
-                                </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
+                
                 <div className='pacman'>
 
                     <div className="pacman-photo">
